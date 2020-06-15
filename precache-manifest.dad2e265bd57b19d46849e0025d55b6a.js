3da8fb73bcc3d57f174a117d1804160b3f5bed73
Oscar <oscar.ramos@ucsp.edu.pe>
2020-06-15 02:02:38 -0500

    Updates

---
+++ precache-manifest.dad2e265bd57b19d46849e0025d55b6a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ca6f834383083b23550a2ab9024ed0",
+    "url": "/github-profiles/index.html"
+  },
+  {
+    "revision": "d0f90aea570ac4fa7141",
+    "url": "/github-profiles/static/js/2.f70add0f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/github-profiles/static/js/2.f70add0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bc14094ccb831ac4924",
+    "url": "/github-profiles/static/js/main.68f467f4.chunk.js"
+  },
+  {
+    "revision": "e67d6126aef6d4328ca4",
+    "url": "/github-profiles/static/js/runtime-main.2ebafa93.js"
+  },
+  {
+    "revision": "583ef3775642d786afdd8af6cc7755f1",
+    "url": "/github-profiles/static/media/Followers.583ef377.svg"
+  },
+  {
+    "revision": "f311b4dc603114ed229bd04e4b078caf",
+    "url": "/github-profiles/static/media/Following.f311b4dc.svg"
+  },
+  {
+    "revision": "da7b7a613a4ff8da339e3fe88d126690",
+    "url": "/github-profiles/static/media/Repositories.da7b7a61.svg"
+  },
+  {
+    "revision": "5aa41bcd0e4f71a7547a3ad4da87e846",
+    "url": "/github-profiles/static/media/RepositoriesLight.5aa41bcd.svg"
+  },
+  {
+    "revision": "7441555e575ea77eb676c290ec53535c",
+    "url": "/github-profiles/static/media/Stars.7441555e.svg"
+  }
+]);
